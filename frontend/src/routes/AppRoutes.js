7702188a6c19106ea3bc5d1cb2e1f9e39bc7c819
Leonardo Reuter <165732957+leozreuter@users.noreply.github.com>
2025-08-23 20:40:55 +0000

    add routes

---
+++ frontend/src/routes/AppRoutes.js
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+import * as Pages from "../pages";
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Pages.Home />} />
+      <Route path="/user" element={<Pages.User />} />
+
+      <Route path="*" element={<h2>Página não encontrada</h2>} />
+    </Routes>
+  );
+}
