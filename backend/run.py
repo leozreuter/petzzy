650af7e0071a1bec9ca55eff2c865aeb81ded5d5
Leonardo Reuter <165732957+leozreuter@users.noreply.github.com>
2025-08-24 20:28:33 +0000

    feat: add development infra

--- backend/run.py
+++ backend/run.py
@@ -1,7 +1,9 @@
 from app import create_app
+from infra.scripts import services
 
 app = create_app()
 
 if __name__ == "__main__":
     # Run the Flask app
+    services.up()
     app.run(debug=True, host='0.0.0.0')
