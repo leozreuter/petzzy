6c0e1b466632781e589930159d51ee7cec21ee16
Leonardo Reuter <165732957+leozreuter@users.noreply.github.com>
2025-08-30 17:47:55 +0000

    add `Logo` component and images

---
+++ frontend/src/components/logo/Logo.jsx
@@ -0,0 +1,23 @@
+export default function Logo({
+  type = "text",
+  variant = "primary",
+  size = "medium", // default
+}) {
+  // Map de tamanhos para arquivos de imagem
+  const sizeMap = {
+    small: "30",
+    medium: "96",
+    large: "150",
+    xlarge: "200",
+  };
+
+  const sizeSuffix = sizeMap[size] || sizeMap["xlarge"];
+
+  const src_defined =
+    type === "only"
+      ? `/logo_only_${sizeSuffix}.png`
+      : `/logo_${sizeSuffix}.png`;
+  return (
+    <img className={`img ${variant}`} src={src_defined} alt="Petzzy Logo" />
+  );
+}
