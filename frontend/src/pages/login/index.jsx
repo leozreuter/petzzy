8a041ff78ae31642be2eff71a69a1ce9cd12a01b
Leonardo Reuter <165732957+leozreuter@users.noreply.github.com>
2025-08-31 22:54:00 +0000

    update `Login` page to use `InputIcon`

--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
-import { useNavigate } from "react-router-dom";
-import Logo from "../../components/logo/Logo";
 
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+import Logo from "../../components/logo/Logo";
+import InputIcon from "../../components/input/InputIcon";
+
+import { KeyRound, Mail } from "lucide-react";
+
 export default function LoginPage() {
-  const navigate = useNavigate();
   const [form, setForm] = useState({ email: "", password: "" });
   const [error, setError] = useState("");
 
@@ -37,91 +39,87 @@ export default function LoginPage() {
   }
 
   return (
-    <div className="min-h-screen flex items-center flex-col justify-center bg-gradient-to-br from-blue-50 to-blue-100 p-4">
-      <motion.div
-        initial={{ opacity: 0, y: 40 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.6 }}
-        className="w-full max-w-md"
-      >
-        <div className="bg-white rounded-2xl shadow-lg p-6">
-          <div className="flex flex-col items-center mb-6">
-            <Logo size="xlarge" className="w-15 h-15" />
+    (document.title = "Faça Login | Petzzy</title>"),
+    (
+      <div className="min-h-screen flex items-center flex-col justify-center bg-gradient-to-br from-blue-100 to-blue-200 p-4">
+        <motion.div
+          initial={{ opacity: 0, y: 40 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6 }}
+          className="w-full max-w-md"
+        >
+          <div className="bg-white rounded-2xl shadow-lg p-6">
+            <div className="flex flex-col items-center mb-6">
+              <Logo size="xlarge" className="w-15 h-15" />
+            </div>
+
+            <form onSubmit={handleSubmit} className="space-y-4">
+              <div>
+                <label className="block text-sm font-medium text-gray-700 mb-1">
+                  E-mail
+                </label>
+                <InputIcon
+                  type="email"
+                  name="email"
+                  placeholder="email@exemplo.com"
+                  value={form.email}
+                  onChange={handleChange}
+                  required
+                  icon={Mail}
+                  error={error}
+                />
+              </div>
+
+              <div>
+                <label className="block text-sm font-medium text-gray-700 mb-1">
+                  Senha
+                </label>
+                <InputIcon
+                  type="password"
+                  name="password"
+                  placeholder="********"
+                  value={form.password}
+                  onChange={handleChange}
+                  required
+                  icon={KeyRound}
+                  error={error}
+                />
+              </div>
+
+              <button
+                type="submit"
+                className="w-full rounded-xl bg-blue-600 hover:bg-petzzy-blue2 text-white font-medium py-2 transition"
+              >
+                Entrar
+              </button>
+              {error && (
+                <p className="text-red-500 font-medium text-sm mt-1 text-center">
+                  {error}
+                </p>
+              )}
+            </form>
+            <div className="text-center mt-4 text-sm ">
+              <a
+                href="/recuperar-senha"
+                className="text-center text-blue-600 hover:underline hover:text-petzzy-blue2 font-medium"
+              >
+                Esqueceu a senha?
+              </a>
+            </div>
           </div>
-
-          <form onSubmit={handleSubmit} className="space-y-4">
-            <div>
-              <label className="block text-sm font-medium text-gray-700">
-                E-mail
-              </label>
-              <input
-                type="email"
-                name="email"
-                placeholder="seuemail@exemplo.com"
-                value={form.email}
-                onChange={handleChange}
-                required
-                className={`mt-1 w-full rounded-lg shadow-sm focus:outline-none ring-1 ring-petzzy-blue focus:ring-2 focus:ring-offset-1 p-2 ${
-                  error
-                    ? "border-red-500 focus:border-red-500 focus:ring-red-600 bg-red-200"
-                    : "border-gray-300 focus:border-petzzy-blue focus:ring-petzzy-blue2"
-                }`}
-              />
-            </div>
-
-            <div>
-              <label className="block text-sm font-medium text-gray-700">
-                Senha
-              </label>
-              <input
-                type="password"
-                name="password"
-                placeholder="********"
-                value={form.password}
-                onChange={handleChange}
-                required
-                className={`mt-1 w-full rounded-lg shadow-sm focus:outline-none ring-1 ring-petzzy-blue focus:ring-2 focus:ring-offset-1 p-2 
-                ${
-                  error
-                    ? "border-red-500 focus:border-red-500 focus:ring-red-600 bg-red-200"
-                    : "border-gray-300 focus:border-petzzy-blue focus:ring-petzzy-blue2"
-                }`}
-              />
-            </div>
-
-            <button
-              type="submit"
-              className="w-full rounded-xl bg-petzzy-blue hover:bg-petzzy-blue2 text-white font-medium py-2 transition"
-            >
-              Entrar
-            </button>
-            {error && (
-              <p className="text-red-500 font-medium text-sm mt-1 text-center">
-                {error}
-              </p>
-            )}
-          </form>
-          <div className="text-center mt-4 text-sm ">
+          <div className="text-center mt-4 text-sm text-gray-500">
+            Novo na Petzzy?{" "}
             <a
-              href="/recuperar-senha"
-              className="text-center text-petzzy-blue hover:underline hover:text-petzzy-blue2 font-medium"
+              href="/cadastro"
+              className="text-blue-600 hover:underline hover:text-petzzy-blue2 font-medium"
             >
-              Esqueceu a senha?
+              Cadastre-se
             </a>
           </div>
-        </div>
-        <div className="text-center mt-4 text-sm text-gray-500">
-          Novo na Petzzy?{" "}
-          <a
-            href="/cadastro"
-            className="text-petzzy-blue hover:underline hover:text-petzzy-blue2 font-medium"
-          >
-            Cadastre-se
-          </a>
-        </div>
-        <ToastContainer position="top-right" autoClose={1500} />
-      </motion.div>
-      <br></br>
-    </div>
+          <ToastContainer position="top-right" autoClose={1500} />
+        </motion.div>
+        <br></br>
+      </div>
+    )
   );
 }
