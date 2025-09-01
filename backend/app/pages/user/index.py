16ce16b718e3654eba03baf6a7c32f26e275d08e
Rafael Zanella <156085494+Rafazanella01@users.noreply.github.com>
2025-09-01 23:58:05 +0000

    Add rotas inativar/ativar

--- backend/app/pages/user/index.py
+++ backend/app/pages/user/index.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
-from app.models.usuarios import Usuario
+from app.models.usuarios import Usuario, ValidationError
+from flask_jwt_extended import jwt_required
 
 bp = Blueprint('user', __name__, url_prefix="/api/v1/user")
 
@@ -10,7 +11,7 @@ async def criarUsuario():
     """
     try:
         data = request.get_json()
-        user = Usuario.criar(data)
+        user = Usuario.criarUsuario(data)
         return jsonify(user.retornaDicionario()), 201
     except Exception as err:
         return jsonify(err.to_dict()),err.to_dict().get("status_code")
@@ -26,3 +27,54 @@ async def listarUsuarios():
         return jsonify(usuarios), 200
     except Exception as err:
         return jsonify(err.to_dict()),err.to_dict().get("status_code")
+    
+@bp.route('/inativar', methods=['POST'], strict_slashes=False)
+#@jwt_required()
+def inativarUsuario():
+    data = request.get_json()
+    if not data:
+        return jsonify({"error": "Corpo da requisição não pode ser vazio."}), 400
+    try:
+        id = data.get('id')
+        if not id:
+            raise ValidationError("É necessário fornecer o 'id' do usuário.")
+
+        usuarioSelecionado = Usuario.procuraUsuarioPorId(id)
+        if not usuarioSelecionado:
+            return jsonify({"error": "Usuário não encontrado."}), 404
+
+        usuarioSelecionado.inativarUsuario()
+
+        return jsonify({"message": f"Usuário '{usuarioSelecionado.nome}' foi inativado com sucesso."}), 200
+
+    except ValidationError as e:
+        return jsonify({"error": str(e)}), 400 
+    
+    except Exception as e:
+        return jsonify({"error": f"Ocorreu um erro interno no servidor.{e}"}), 500
+    
+@bp.route('/ativar', methods=['POST'], strict_slashes=False)
+#@jwt_required()
+def ativarUsuario():
+    data = request.get_json()
+    if not data:
+        return jsonify({"error": "Corpo da requisição não pode ser vazio."}), 400
+    try:
+        id = data.get('id')
+        if not id:
+            raise ValidationError("É necessário fornecer o 'id' do usuário.")
+
+        usuarioSelecionado = Usuario.procuraUsuarioPorId(id)
+        if not usuarioSelecionado:
+            return jsonify({"error": "Usuário não encontrado."}), 404
+
+        usuarioSelecionado.ativarUsuario()
+
+        return jsonify({"message": f"Usuário '{usuarioSelecionado.nome}' foi ativado com sucesso."}), 200
+
+    except ValidationError as e:
+        return jsonify({"error": str(e)}), 400 
+    
+    except Exception as e:
+        return jsonify({"error": f"Ocorreu um erro interno no servidor.{e}"}), 500
+    
